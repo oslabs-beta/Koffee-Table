308fa8c89cd88f43471d22b4a461a19bcbd3cb93
Jonathan Valdes <jonathanvaldes@Jonathans-Air.hitronhub.home>
2023-04-11 19:19:15 -0400

    main-page

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,30 @@
-const express = require("express");
-const path = require("path");
-const producerController= require('../kafka/producer')
+const express = require('express');
+const path = require('path');
+const producerController = require('../kafka/producer');
 
 const app = express();
 app.use(express.json());
 
-
 //serve main page of application
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/index.html"));
-})
-app.use("/build", express.static(path.resolve(__dirname, "../build")));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/index.html'));
+});
+app.use('/build', express.static(path.resolve(__dirname, '../build')));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"), err => {
-    if(err){
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'), (err) => {
+    if (err) {
       console.log(err);
     }
-  })
-})
+  });
+});
 
+app.post('/getCluster', (req, res) => {
+  res.json('cluster Connected');
+});
 
-
-app.post("/", producerController.addMsg, (req, res) => {
+app.post('/', producerController.addMsg, (req, res) => {
   return res.sendStatus(200);
 });
 
-app.listen(3000, () => console.log("listening to 3000"));
+app.listen(3000, () => console.log('listening to 3000'));
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,23 +1,116 @@
 import React, { useState, useEffect } from 'react';
-// import PartitionGraph from './PartitionGraph.jsx';
+import Connect from './Connect.jsx';
+import BasicClusterInfo from './BasicClusterInfo.jsx';
+
+
+const example = {
+  topics: [
+    {
+      name: 'Users',
+      partitions: [
+        {
+          partitionErrorCode: 0,
+          partitionId: 0,
+          leader: 2,
+          replicas: [2],
+          isr: [2],
+          offlineReplicas: [],
+        },
+        {
+          partitionErrorCode: 0,
+          partitionId: 1,
+          leader: 3,
+          replicas: [3],
+          isr: [3],
+          offlineReplicas: [],
+        },
+      ],
+    },
+    {
+      name: 'test-topic2',
+      partitions: [
+        {
+          partitionErrorCode: 0,
+          partitionId: 0,
+          leader: 3,
+          replicas: [3],
+          isr: [3],
+          offlineReplicas: [],
+        },
+        {
+          partitionErrorCode: 0,
+          partitionId: 2,
+          leader: 2,
+          replicas: [2],
+          isr: [2],
+          offlineReplicas: [],
+        },
+        {
+          partitionErrorCode: 0,
+          partitionId: 1,
+          leader: 1,
+          replicas: [1],
+          isr: [1],
+          offlineReplicas: [],
+        },
+      ],
+    },
+  ],
+};
+
+
+
+
 
 function Navbar() {
+  const [connecting, setConnecting] = useState(false);
+  const [partitionsDisplay, setPartionsDisplay] = useState(false);
+  const [transitTimesDisplay, setTransitTimesDisplay] = useState(false);
+  
   const connecter = () => {
-    //this is where we run the connect function that the other team has built
-    console.log('trying to connect...');
+    goHome();
+    setConnecting(true);
+  };
+
+  const displayPartitions = () => {
+    goHome();
+    setPartionsDisplay(true);
+  };
+
+  const displayTransitTime = () => {
+    goHome();
+    setTransitTimesDisplay(true);
+  };
+
+  const goHome = () => {
+    setConnecting(false);
+    setPartionsDisplay(false);
+    setTransitTimesDisplay(false);
   };
 
   return (
     <div className='wrapper'>
       <div className='navBar'>
+      <button className='button homebutton' onClick={goHome}>
+         Home
+        </button>
         <button className='button connectButton' onClick={connecter}>
           Connect to Kafka Cluster
         </button>
-        <button className='button partitionButton'>Display Partitions</button>
+        <button className='button partitionButton' onClick={displayPartitions}>Display Partitions</button>
+        <button className='button displayTransitTimeButton' onClick={displayTransitTime}>Display Transit Time</button>
       </div>
-      <h1 className='title'>The Kafka Visualizer</h1>
+      <div className="ConnectToCluster">
+        <h1 className='title'>Koffee Table</h1>
+        <div>
+          {connecting ? <Connect /> : <div></div>}
+          {partitionsDisplay ? <BasicClusterInfo object={example}/> : <div></div>}
+          {transitTimesDisplay ? <div>TRANSIT TIME</div> : <div></div>}
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Navbar;
+
