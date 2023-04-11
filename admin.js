3e53dec0c0f81338c9d86c51402e8b6c762af9a6
Mattholee <matthew0505@gmail.com>
2023-04-11 11:47:15 -0400

    will create docker file to build docker image and incldues admin.js middleware for obtaining metadata from kafka clusters

--- admin.js
+++ admin.js
@@ -2,19 +2,13 @@ const { Kafka } = require('kafkajs');
 
 const admin = {};
 
-inputs = {
-  clientId: 'myapp',
-  broker: ['MATT-XPS:9092'],
-  //   topic: '',
-};
-
 admin.connectAdmin = async (req, res, next) => {
   const { clientId, brokers } = req.body;
-  console.log(clientId);
-  console.log(brokers);
+  // console.log(clientId);
+  // console.log(brokers);
   const kafka = new Kafka({
-    clientId: clientId,
-    brokers: brokers,
+    clientId: clientId, // "my-app"
+    brokers: brokers, // [Joes-Air:9092]
   });
   const admin = kafka.admin();
   await admin.connect();
@@ -26,7 +20,5 @@ admin.connectAdmin = async (req, res, next) => {
 
   return next();
 };
-
-// connectAdmin(inputs.clientId, inputs.broker);
 
 module.exports = admin;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const admin = require('./admin');
 
 app.use(express.json());
 
 //listen for information from admin.js, console.log in this terminal ('messages: ', message)
-app.use('/', admin.connectAdmin, (req, res) => {
+app.use('/connect', admin.connectAdmin, (req, res) => {
   return res.status(200).json(res.locals.topics);
+});
+
+app.use('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(8080, () => console.log('connected to port 8080'));
