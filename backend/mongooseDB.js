ba70a7d77738e8bc3748bb97316a43fe1effc395
Joe Ostrow <josephostrow@joes-air.mynetworksettings.com>
2023-05-03 14:16:33 -0400

    add env variable

--- backend/mongooseDB.js
+++ backend/mongooseDB.js
@@ -1,58 +1,60 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const dotenv = require('dotenv');
 
-const URI = 'mongodb+srv://KoffeeTable:hrVYGnzwzprjzp4J@cluster0.0xvkesc.mongodb.net/test';
+dotenv.config();
+
+//add your own MongoDB connection string as an environment variable
+const URI = `${process.env.URI}`;
 const SALT_WORK_FACTOR = 12;
 
 //connect to database
-mongoose.connect(URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  dbName: 'Cluster0'
-})
-.then(()=>console.log('connected to mongooseDb'))
-.catch((err) => console.log('error in connection to mongooseDB', err));
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'Cluster0',
+  })
+  .then(() => console.log('connected to mongooseDb'))
+  .catch((err) => console.log('error in connection to mongooseDB', err));
 
 const Schema = mongoose.Schema;
 
 //establish user schema
 const userSchema = new Schema({
-  username: {type: String, required: true},
-  password: {type: String, required: true},
-  clientID: {type: String, required: false},
-  hostName: {type: String, required: false},
-  port: {type: Number, required: false},
-})
-
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  clientID: { type: String, required: false },
+  hostName: { type: String, required: false },
+  port: { type: Number, required: false },
+});
 
 //hash password on user creation before it is saved in database
-userSchema.pre('save', function(next){
+userSchema.pre('save', function (next) {
   const user = this;
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(saltError, salt) {
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (saltError, salt) {
     if (saltError) {
       return next(saltError);
-    }
-    else{
+    } else {
       bcrypt.hash(user.password, salt, function (hashError, hash) {
         if (hashError) {
           return next(hashError);
-        }
-        else{
+        } else {
           user.password = hash;
           return next();
         }
-      })
+      });
     }
-  })
-})
+  });
+});
 
 //method to compare password on login
-userSchema.methods.passwordCheck = function(input, cb) {
-  bcrypt.compare(input, this.password, function(err, result){
+userSchema.methods.passwordCheck = function (input, cb) {
+  bcrypt.compare(input, this.password, function (err, result) {
     if (err) return cb(err);
     return cb(null, result);
-  })
-}
+  });
+};
 
 const User = mongoose.model('users', userSchema);
 
